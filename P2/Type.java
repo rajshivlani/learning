d8e85114fe6e4230a975e4d11c381e1c3b6c9b5b
rajshivlani <142081305+rajshivlani@users.noreply.github.com>
2026-06-18 15:32:51 +0530

    day 2
    type conversion and casting

---
+++ P2/Type.java
@@ -0,0 +1,10 @@
+class Typec {
+
+    public static void main(String a[]){
+        float input =10.5f;
+        int output = (int)input; //explicit type casting  when converting from float to int lost decimal part 
+        System.out.println(input);
+        System.out.println(output);
+    }
+
+}

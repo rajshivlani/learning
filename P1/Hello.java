7869174843a9bf44b7f698c0bc1bebb16f88bae9
rajshivlani <142081305+rajshivlani@users.noreply.github.com>
2026-06-18 14:24:07 +0530

    upload

---
+++ P1/Hello.java
@@ -0,0 +1,26 @@
+class Hello {
+
+    public static void main(String a[])
+    {
+        System.out.println("hiii");
+        byte num = 10;
+
+        double number1 = 5.7;
+        float number2 = 5.7f;
+        
+        long num2 = 102000000000000L;
+        
+        float result = (num + number2);
+
+        boolean guess = true;
+        
+        char text = 'O';
+
+        System.out.println(number1);
+        System.out.println(number2);
+        System.out.println(result);
+        System.out.println(num2);
+        System.out.println(guess);
+        System.out.println(text);
+    }
+}
---
+++ P2/Hello.java
@@ -0,0 +1,16 @@
+class Hello{
+
+    public static void main(String a[]){
+
+        // literals
+        double test = 10e9;
+        double test2 = 10;
+        double test3 = test2 + 100;
+        test3++; //increased by 1
+        System.out.println("Hello World");
+        System.out.println(test);
+        System.out.println(test2);
+        System.out.println(test3);
+    }
+
+}

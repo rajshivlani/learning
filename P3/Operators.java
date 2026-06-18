88a933e8968e4d71a4a7e7dda12de4c83626851d
rajshivlani <142081305+rajshivlani@users.noreply.github.com>
2026-06-18 23:23:58 +0530

    Day 3
    Learnt basic operators

---
+++ P3/Operators.java
@@ -0,0 +1,16 @@
+class Operators {
+
+    public static void main(String a[])
+    {
+        int num1 = 10;
+        int num2 = 20;
+
+        int num3 = num1 + num2; 
+
+        num3 /= 2;
+
+        System.out.println(num3);
+        System.out.println(num1 + num2 + num3);
+    }
+
+}

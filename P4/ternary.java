c2bc0f9859148c786904c81a165f42a38629eb8c
rajshivlani <142081305+rajshivlani@users.noreply.github.com>
2026-06-19 16:45:06 +0530

    Day 4 - 2
    Ternary operator
    if, else if, else statement
    Switch statement

---
+++ P4/ternary.java
@@ -0,0 +1,15 @@
+class ternary{
+    public static void main(String[] args) {
+        int num1 = 2;
+        int num2 = 5;
+
+        String result = num1==num2 ? "Equal" : "Not Equal";
+        System.out.println("The numbers are: " + result);
+
+        int number1 = 2;
+        int number2 = 2;
+
+        String result2 = (number1==number2) ? "Equal" : "Not Equal";
+        System.out.println("The numbers are: " + result2);
+    }
+}
